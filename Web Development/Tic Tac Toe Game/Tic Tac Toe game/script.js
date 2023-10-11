c551d38bf0db2ffb8098256ecb08c4ad9d87b74b
Gyanshu Kumar <119155250+Gyanshu-Kumar@users.noreply.github.com>
2023-10-12 03:13:17 +0530

    Update script.js
    make buttons larger and change the background 
    which looks cool

--- Web Development/Tic Tac Toe Game/Tic Tac Toe game/script.js
+++ Web Development/Tic Tac Toe Game/Tic Tac Toe game/script.js
@@ -1,69 +1,63 @@
-console.log("Welcome to Tic Tac Toe")
-let music = new Audio("music.mp3")
-let audioTurn = new Audio("ting.mp3.wav")
-let gameover = new Audio("gameover.mp3.mp3")
-let turn = "X"
-let isgameover = false;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tic Tac Toe</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+    <ul>
+        <li>MyTicToe.com</li>
+    </ul>
+    </nav>
+    <hr>
 
-//Function to change the turn 
-
-const changeTurn = ()=>{
-    return turn === "X"?"0": "X"
-}
-
-//Function to check for a win
-
-const checkWin = ()=>{
-    let boxtext = document.getElementsByClassName('boxtext');
-      let wins = [
-        [0 , 1 , 2 ],
-        [3 , 4 , 5 ],
-        [6 , 7 , 8 ],
-        [0 , 3 , 6 ],
-        [1 , 4 , 7 ],
-        [2 , 5 , 8 ],
-        [0 , 4 , 8 ],
-        [2 , 4 , 6 ],
-      ]
-      wins.forEach(e =>{
-        if((boxtext[e[0]].innerText == boxtext[e[1]].innerText) && (boxtext[e[2]].innerText == boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== '') ){
-            document.querySelector('.info').innerText = boxtext[e[0]].innerText +  " won"
-            isgameover = true
-            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px"
+    <div class="gameContainer">
+        <div class="container">
+          
+          
+        
+            <div class="box bt-0 bl-0"><span class="boxtext"></span></div>
             
-         }
-      })
-}
-
-// Game Logic
-let boxes = document.getElementsByClassName("box");
-Array.from(boxes).forEach(element =>{
-    let boxtext = element.querySelector('.boxtext');
-    element.addEventListener('click' , ()=>{
-        if(boxtext.innerText === ''){
-            boxtext.innerText = turn;
-            turn = changeTurn();
-            audioTurn.play();
-            checkWin();
-            if(!isgameover){
-                document.getElementsByClassName("info")[0].innerText = " Turn for " +  turn;
-            }
+            <div class="box bt-0"><span class="boxtext"></span></div>
+            <div class="box bt-0 br-0"><span class="boxtext"></span></div>
+            <div class="box bl-0"><span class="boxtext"></span></div>
+            <div class="box"><span class="boxtext"></span></div>
+            <div class="box br-0"><span class="boxtext"></span></div>
+            <div class="box bl-0 bb-0"><span class="boxtext"></span></div>
+            <div class="box bb-0"><span class="boxtext"></span></div>
+            <div class="box bb-0 br-0"><span class="boxtext"></span></div>
+        </div>
+        <div class="gameInfo">
+          <hr>
+            <h1>Welcome to Tic Tac Toe Game</h1>
             
-        }
-    })
-})
+            <div>
+                <span class="info">Turn for X</span>
+                
+                <button id="reset">Reset</button>
+                <!-- <main class="main">
+                  <button class="btn">Reset</button>
+           </main> -->
+            </div>
+            <div class="imgbox">
+                <img src="./images/excited-spin.gif" alt="">
+            </div>
+        </div>
+    </div>
 
-//Add on click listener to reset button
 
- reset.addEventListener('click' , ()=>{
-    let boxtexts = document.querySelectorAll('.boxtext');
-    Array.from(boxtexts).forEach(element => {
-        element.innerText = ""
-    });
-    turn = " 0";
-     
-    isgameover = false
-    
-        document.getElementsByClassName("info")[0].innerText = "Turn for" + turn;
-        document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px"
-})
+<script src = "script.js"></script>
+<hr>
+<footer>
+<div class="footer-rights">
+  Copyright &#169; www.MyTicTacToe.com | All rights reserved
+  
+</div>
+</footer>
+
+</body>
+</html>
